c7adbbafa89334f1e9796684e4793c5d1faed0f8
talalahmed409 <talalahmed409@gmail.com>
2026-05-13 03:27:16 +0300

    added macos/windows accel profiles

--- hypr/.config/hypr/hyprland/input.lua
+++ hypr/.config/hypr/hyprland/input.lua
@@ -13,7 +13,7 @@ hl.config({
 		touchpad = {
 			natural_scroll = true,
 			disable_while_typing = true,
-			scroll_factor = 0.45,
+			scroll_factor = 0.07,
 			drag_lock = false,
 		},
 	},
@@ -45,4 +45,6 @@ hl.device({
 	name = "synps/2-synaptics-touchpad",
 	sensitivity = 0.2,
 	accel_profile = "adaptive",
+	-- accel_profile = "custom  0.2144477506 0.000 0.307 0.615 1.077 1.539 2.002 2.505 3.208 3.910 4.613 5.315 6.018 6.720 7.423 8.125 8.828 9.530 10.233 10.935 12.387", --windows
+	-- accel_profile = "custom 0.5 0.000 0.053 0.115 0.189 0.280 0.391 0.525 0.687 0.880 1.108 1.375 1.684 2.040 2.446 2.905 3.422 4.000 4.643 5.355 6.139", macos
 })
